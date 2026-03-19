7a66569eaf858d519f30239fc53be0962db28197
Kawus <kawus.nouri@gmail.com>
2026-03-19 13:26:42 +0100

    fix: apply font-sans class to body so Geist renders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-cabin-dark text-cabin-cream">
+      <body className="min-h-full flex flex-col bg-cabin-dark text-cabin-cream font-sans">
         {children}
       </body>
     </html>
